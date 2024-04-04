88eb7e0b461f806800f0b9d6d3ee7beebf6add09
Junha7777 <junha@dgsw.hs.kr>
2024-04-04 20:59:26 +0900

    Commit Title here
    Featured
    + Launch Screen
    + First View
    + Calender View

--- Cl!ck/ContentView.swift
+++ Cl!ck/ContentView.swift
@@ -10,12 +10,44 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            HStack {
-                Text("임귀당귀")
-                    .font(.title)
-                Image(systemName: "person")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
+            VStack {
+                Image("LSCon")
+                    .resizable()
+                    .frame(width: 237.9228302472, height: 100)
+                Divider()
+                    .frame(width:300)
+                    .background(Color.black)
+                Text("오직 학생들에 의한, 학생들을 위한")
+                    .padding(.top, 15)
+                Text("수행평가 관리 플랫폼 C!ick")
+            }
+            .padding(.top, 50)
+            Button(action: {
+                
+            }){
+                HStack {
+                    Image("DAuth-Icon")
+                        .resizable()
+                        .frame(width:24, height: 24)
+                    Text("DAuth로 시작하기")
+                        .foregroundStyle(Color.black)
+                        .bold()
+                }
+                .padding()
+                .frame(width: 300)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.black, lineWidth: 1)
+                    )
+                .padding(.top, 300)
+            }
+            Button(action: {
+                
+            }){
+                Text("회원 가입을 진행할까요?")
+                    .foregroundStyle(Color.black)
+                    .bold()
+                    .font(.system(size:13))
             }
         }
         .padding()
