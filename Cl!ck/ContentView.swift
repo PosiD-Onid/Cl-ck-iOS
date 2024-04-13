ff576aa393abb6987b34f999112628e4c188967e
uoto00 <140079982+uoto00@users.noreply.github.com>
2024-04-13 22:04:22 +0900

    Appicon, ContentView
    앱 아이콘 추가, 시작화면 추가

--- Cl!ck/ContentView.swift
+++ Cl!ck/ContentView.swift
@@ -1,24 +1,52 @@
 //
 //  ContentView.swift
-//  Cl!ck
+//  Click01
 //
-//  Created by Junha on 3/21/24.
+//  Created by 이다경 on 4/4/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Text("임귀당귀")
-                    .font(.title)
-                Image(systemName: "person")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
+        GeometryReader{ geometryReader in
+            NavigationView {
+                VStack(spacing: 0) {
+                    Spacer()
+                        .frame(height: geometryReader.size.height / 8)
+                    Image("ClickLogo")
+                        .resizable()
+                        .frame(width: geometryReader.size.width / 2.2, height: geometryReader.size.height / 10)
+                    
+                    Divider()
+                        .frame(width: geometryReader.size.width / 1.4, height: 1)
+                        .overlay(Color("gray400"))
+                        .padding()
+                    
+                    VStack(alignment: .center) {
+                        Text("오직 학생들에 의한, 학생들을 위한")
+                        Text("수행평가 관리 플랫폼 C!ick")
+                    }
+                    .font(.system(size: geometryReader.size.width / 21))
+                    .foregroundColor(Color("gray800"))
+                    Spacer()
+                        .frame(height: geometryReader.size.height / 2.2)
+                    NavigationLink(destination: MainCalendarView()) {
+                        HStack {
+                            Image("DodamLogo")
+                            Text("DAuth로 시작하기")
+                                .foregroundStyle(.black)
+                                .font(.system(size: geometryReader.size.width / 23))
+                        }
+                        .frame(width: geometryReader.size.width / 1.2, height: geometryReader.size.height / 13)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.black, lineWidth: 1)
+                        )
+                    }
+                }
             }
         }
-        .padding()
     }
 }
 
